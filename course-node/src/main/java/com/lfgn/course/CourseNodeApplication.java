4c20592999cb9dc3408609e3566c3c8bd04e89d3
Frances <delacruzfrances0720@gmail.com>
2025-04-11 13:28:48 +0800

    fix: fixed database access

--- course-node/src/main/java/com/lfgn/course/CourseNodeApplication.java
+++ course-node/src/main/java/com/lfgn/course/CourseNodeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.lfgn.course.repository")  // Enables scanning of repositories
-@EntityScan(basePackages = "com.lfgn.course.model")  // Enables scanning of model entities
+//@EnableJpaRepositories(basePackages = "com.lfgn.course.repository")  // Enables scanning of repositories
+//@EntityScan(basePackages = "com.lfgn.course.model")  // Enables scanning of model entities
 public class CourseNodeApplication {
 
 	public static void main(String[] args) {
--- course-node/src/main/java/com/lfgn/course/model/Course.java
+++ course-node/src/main/java/com/lfgn/course/model/Course.java
@@ -9,7 +9,32 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idcourses;
 
+    @Column(name = "coursename")
     private String courseName;
+    @Column(name = "facultyID")
     private int facultyID;
 
+    public int getIdcourses() {
+        return idcourses;
+    }
+
+    public void setIdcourses(int idcourses) {
+        this.idcourses = idcourses;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getFacultyID() {
+        return facultyID;
+    }
+
+    public void setFacultyID(int facultyID) {
+        this.facultyID = facultyID;
+    }
 }
