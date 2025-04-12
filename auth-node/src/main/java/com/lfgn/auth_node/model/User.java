0253948dfa6415b8ddce1c754173bf740bb7ed5a
Frances <delacruzfrances0720@gmail.com>
2025-04-12 15:56:29 +0800

    feat: added auth-node

---
+++ auth-node/src/main/java/com/lfgn/auth_node/model/User.java
@@ -0,0 +1,41 @@
+package com.lfgn.auth_node.model;
+
+public abstract class User {
+    protected Long id;
+    protected String username;
+    protected String password;
+    protected String firstName;
+    protected String lastName;
+
+    // Constructors
+    public User() {}
+
+    public User(Long id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters and setters
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
---
+++ auth-node/src/test/java/com/lfgn/auth_node/AuthNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfgn.auth_node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
